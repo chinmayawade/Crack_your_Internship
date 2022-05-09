875f961c59bf95a147b80549c4badcfd6c21705a
Chinmay Awade <90987160+chinmayawade@users.noreply.github.com>
2022-05-09 13:45:14 +0530

    Create Implement Queue using Stacks.java

---
+++ Day 2/Implement Queue using Stacks.java
@@ -0,0 +1,36 @@
+class MyQueue {
+
+    private Stack<Integer> s1;
+    private Stack<Integer> s2;
+    
+    public MyQueue() {
+        s1= new Stack<Integer>();
+        s2= new Stack<Integer>();
+    }
+    
+    public void push(int x) {
+       
+        while(!s1.empty()){
+            s2.push(s1.pop());
+        }
+        s1.push(x);
+        while(!s2.empty()){
+            s1.push(s2.pop());
+        }
+    }
+    
+    public int pop() {
+        return s1.pop();
+    }
+    
+    public int peek() {
+
+     
+    return s1.peek();
+        
+    }
+    
+    public boolean empty() {
+        return s1.empty() ;
+    }
+}
